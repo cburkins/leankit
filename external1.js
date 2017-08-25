a49ae52494408d6663422188bc5747db19bd4b9b
Chad Burkins <chad.burkins@gmail.com>
2017-08-25 09:09:27 -0400

    cardCreated

--- external1.js
+++
@@ -1,6 +0,0 @@
-
-// Create an object called greetings, and assign methods defined in file hello.js
-var greetings = require("./hello.js");
-
-console.log(greetings.sayHello());
-console.log(greetings.sayGoodbye());
